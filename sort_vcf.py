7a09f73f0d0a5bc071d5d22e446739410fe6e4a4
chenchengfly <hengbenxianfeng@163.com>
2018-10-07 17:19:28 +0800

    Update sort_vcf.py

--- sort_vcf.py
+++ sort_vcf.py
@@ -11,7 +11,6 @@ if len(sys.argv) < 3:
 	exit(1)
 dic1 = {}
 dic2 = {}
-head = []
 chr_list = ["chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY","chrM"]
 in_vcf = sys.argv[1]
 out_vcf = sys.argv[2]
